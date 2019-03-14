331524870f11e06a1d05d426bbf2ea60872e2cfe
Balázs Kis <kisbalazs90@gmail.com>
2019-03-14 15:21:42 +0100

    Download and unzip file

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,39 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-  }
-  
-  exports.default = defaultTask
+const axios = require('axios');
+const fs = require('fs');
+const unzip = require('unzip');
+const extensionUrl = 'https://github.com/kisbalazs90/gulp-test/archive/master.zip';
+
+async function startInstall(cb) {
+
+  const gulpFileStream = await downloadTask();
+  const savedFile = await saveFile(gulpFileStream);
+  fileUnzip(savedFile);
+
+  cb();
+}
+
+async function downloadTask() {
+  const gulpFilePath = extensionUrl;
+
+  return await axios({
+    method: 'get',
+    url: gulpFilePath,
+    responseType: 'stream'
+  }).then(response => response.data);
+}
+
+async function saveFile(gulpFileStream) {
+  return await new Promise(resolve => {
+    const gulpWriteStream = fs.createWriteStream('./master.zip');
+    gulpWriteStream.on('close', () => { resolve(gulpWriteStream.path); });
+    gulpFileStream.pipe(gulpWriteStream);
+  });
+}
+
+function fileUnzip(zipFile) {
+  return fs.createReadStream(zipFile).pipe(unzip.Extract({ path: './tmp' }));
+}
+
+
+
+exports.default = startInstall
